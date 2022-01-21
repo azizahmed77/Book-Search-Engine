83fc3c7a1c6c685fe55946a2bcff6fbddb440faa
aziz <ahmed613@umn.edu>
2022-01-21 13:20:23 -0600

    apollo server set up

--- server/server.js
+++ server/server.js
@@ -1,10 +1,27 @@
 const express = require('express');
-const path = require('path');
+const { ApolloServer } = require('apollo-server-express')
+const { typeDefs, resolvers } = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
-const routes = require('./routes');
+const path = require('path');
+
+//const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4001;
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+   
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+/* app.use(routes); */
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
