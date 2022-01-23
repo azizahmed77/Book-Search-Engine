bf626cd0d3709d12ae0a9055cea8814e801167e1
aziz <ahmed613@umn.edu>
2022-01-23 17:54:12 -0600

    .

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -70,7 +70,7 @@ const SearchBooks = () => {
 
     try {
       const { data } = await saveBook({
-        variables: {newEntry: {bookToSave}}
+        variables: {newEntry: {...bookToSave}}
       });
 
 
